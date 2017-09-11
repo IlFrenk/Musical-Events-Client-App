0610632cd5c7789bb49aee6b43534136737c42d5
Frenk <Frenk>
2017-09-11 12:34:05 +0200

    fixed logut issue and other minor fixes

--- www/js/services.js
+++ www/js/services.js
@@ -6,19 +6,24 @@ angular.module('app.services', [])
 
 .service('orgCompany', [function(){
     var company = [];
-    
+
       var addCompany = function(newObj) {
           company.push(newObj);
       };
-    
+
+      var deleteCompany = function() {
+        company.pop();
+    };
+
       var getCompany = function(){
           return company;
       };
-    
+
       return {
         addCompany: addCompany,
-        getCompany: getCompany
+        getCompany: getCompany,
+        deleteCompany: deleteCompany
       };
-    
+
 
 }]);
